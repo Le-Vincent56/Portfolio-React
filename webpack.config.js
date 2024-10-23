81bc3993782d1b6f7c78189838f2b265f1550ab9
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-10-23 09:50:05 -0400

    Continued webpack file and re-instantiated package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,24 @@ const path = require('path');
 
 module.exports =
 {
-    entry: './client/home.jsx'
+    entry: './client/home.jsx',
+    module : {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    mode: 'production',
+    watchOptions: {
+        aggregateTimeout: 200,
+    },
+    output: {
+        path: path.resolve(__dirname, 'hosted'),
+        filename: '[name]Bundle.js',
+    },
 }
